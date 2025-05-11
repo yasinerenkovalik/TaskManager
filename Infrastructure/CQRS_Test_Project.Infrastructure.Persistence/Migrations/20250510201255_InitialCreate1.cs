1f42c849ca19809e262199d571261643b6cb6f88
yasinerenkovalik <yasinerenkovalik@gmail.com>
2025-05-11 11:21:04 +0300

    day-11

---
+++ Infrastructure/CQRS_Test_Project.Infrastructure.Persistence/Migrations/20250510201255_InitialCreate1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CQRS_Test_Project.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectId",
+                table: "Tasks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectId",
+                table: "Feedbacks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Feedbacks");
+        }
+    }
+}
